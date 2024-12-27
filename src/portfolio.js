26ad3767987b8bae45f8367e6cf511596eb9851f
binodcoder <binodcoder@gmail.com>
2024-12-27 18:41:31 +0000

    some content changed

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,11 +9,12 @@ const about = {
   name: 'Binod Bhandari',
   role: 'Graduate Software Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'MSc Computer Science graduate with distinction and experience developing mobile and web applications using Flutter, Spring Boot, and MySQL. Skilled in delivering innovative, user-focused solutions, including GDPR-compliant platforms. Seeking to contribute technical expertise to an administrative/IT system role in the UK.',
+  resume:
+    'https://1drv.ms/b/c/c4e7f71ed159e7bf/EdzGTkMLvL1IkaYFhuqx27sBkwS6loLtk0jJDLeuK8IoVg?e=AUARhX',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/binodcoder/',
+    github: 'https://github.com/binodcoder',
   },
 }
 
@@ -23,24 +24,24 @@ const projects = [
   {
     name: 'Project 1',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'During my postgraduate program, I built web and mobile applications for a job portal, incorporating advanced security measures such as JWT authentication and AES-256 encryption. The platform features job posting, application management, and user role-based access, ensuring a seamless experience for all stakeholders. I also prioritized GDPR compliance and developed a responsive user interface tailored for both web and mobile platforms, delivering a secure and user-friendly solution.',
+    stack: ['Flutter', 'BLoC', 'Spring Boot'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
   {
     name: 'Project 2',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'I developed a mobile app designed for waitstaff in restaurants and hotels to streamline food order management, enhancing efficiency and accuracy in service. The app gained traction quickly, achieving over 100 downloads, reflecting its practicality and user-focused design.',
+    stack: ['Flutter', 'Provider', 'MVC'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
   {
     name: 'Project 3',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      ' As part of my postgraduate studies, I built an offline blogging platform that empowers users with full control over their posts and media, ensuring privacy and accessibility even without an internet connection. The platform features intuitive navigation and a robust search functionality, enabling users to effortlessly manage and locate their content. This project highlights my ability to design and develop user-centric solutions tailored for seamless offline experiences.',
+    stack: ['Flutter', 'BLoC', 'SQLite'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
@@ -52,15 +53,17 @@ const skills = [
   'HTML',
   'CSS',
   'JavaScript',
-  'TypeScript',
   'React',
-  'Redux',
-  'SASS',
   'Material UI',
   'Git',
   'CI/CD',
   'Jest',
-  'Enzyme',
+  ' Java',
+  'Spring Boot',
+  'MySQL',
+  'MongoDB',
+  'Dart',
+  'Flutter',
 ]
 
 const contact = {
